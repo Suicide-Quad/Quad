169c67e9f72b9ce107d304352ba54487a16aa3cf
Vianney Marticou <vianney.marticou@epita.fr>
2023-04-04 20:57:28 +0200

    [PID] Config PIN Use CubeMX for config pins of TIM2 and TIM5

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -615,7 +615,7 @@ static void MX_TIM5_Init(void)
   htim5.Instance = TIM5;
   htim5.Init.Prescaler = 0;
   htim5.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim5.Init.Period = 4294967295;
+  htim5.Init.Period = 65535;
   htim5.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim5.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   sConfig.EncoderMode = TIM_ENCODERMODE_TI12;
