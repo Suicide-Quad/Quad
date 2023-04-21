c59c617a3a86b570ebc3ce1f21505438f7dca0d5
vianney.marticou <vianney.marticou@epita.fr>
2023-04-21 10:33:21 +0100

    [SERVO] Edit the headers define

--- Core/Inc/servo.h
+++ Core/Inc/servo.h
@@ -1,6 +1,9 @@
-#ifndef SERVO
-#define SERVO
+#ifndef _SERVO_H
+#define _SERVO_H
+#include "main.h"
 
 void servo(double measureLinear, double measureAngular, double orderLinear, double orderAngular);
+
+void initServo();
 
 #endif
