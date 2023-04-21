140e2cb3814f2da9e42d37e89827a888ce85f586
vianney.marticou <vianney.marticou@epita.fr>
2023-04-21 19:03:55 +0100

    [UTILS] Add the MINABS and MAXABS macro

--- Core/Inc/utils.h
+++ Core/Inc/utils.h
@@ -1,9 +1,15 @@
 #ifndef _UTILS_H
 #define _UTILS_H
 #include "odometer.h"
+#include <math.h>
+#define ABS(a) ((a) < 0 ? -(a) : (a))
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MINABS(a,b) (ABS(a) < ABS(b) ? (a) : (b))
+#define MAXABS(a,b) (ABS(a) < ABS(b) ? (b) : (a))
 #define MAX(a,b) ((a) < (b) ? (b) : (a))
 #define CLAMP(a,c,b) MIN(MAX((a),(c)), (b))
+#define DEG_TO_RAD(x) ((x)*M_PI / 180.0F)
+#define RAD_TO_DEG(x) ((x)*180.0F / M_PI)
 
 uint8_t checksum(char* msg);
 MotorSpeed convertPolarSpeed(PolarSpeed speed);
