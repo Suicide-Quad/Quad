e47b4554f5fa710332c180579017b9dd4e4730b9
vianney.marticou <vianney.marticou@epita.fr>
2023-04-24 17:54:15 +0100

    [UTILS] Edit the convert function

--- Core/Src/utils.c
+++ Core/Src/utils.c
@@ -14,16 +14,17 @@ uint8_t checksum(char* msg)
 MotorSpeed convertPolarSpeed(PolarSpeed speed)
 {
     MotorSpeed motor;
-    motor.left = speed.linear - (speed.angular * WHELL_GAPS / 2.0F);
-    motor.right = speed.linear + (speed.angular * WHELL_GAPS / 2.0F);
+    motor.left = speed.linear/WHEEL_RADIUS - speed.angular * (WHELL_GAPS/2*(1/WHEEL_RADIUS));
+    motor.right = speed.linear/WHEEL_RADIUS + speed.angular * (WHELL_GAPS/2*(1/WHEEL_RADIUS));
     return motor;
 }
 
 PolarSpeed convertMotorSpeed(MotorSpeed speed)
 {
     PolarSpeed polar;
-    polar.linear = (speed.left - speed.right) / 2.0F;
-    polar.angular = ((double)WHELL_GAPS / 2.0F) * (speed.right + speed.left) / (speed.right - speed.left);
+    polar.linear = (WHEEL_RADIUS*(speed.left + speed.right) / 2.0F);
+    // FIX : MAYBE NOT RADIUS
+    polar.angular = (speed.right - speed.left)* (WHEEL_RADIUS/WHELL_GAPS);
     return polar;
 }
 
