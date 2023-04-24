52f4b1753e483b02eb22a21ef10eaad9f35d4942
vianney.marticou <vianney.marticou@epita.fr>
2023-04-24 17:56:39 +0100

    [ODOMETER] Rewrite clean define in the headers

--- Core/Inc/odometer.h
+++ Core/Inc/odometer.h
@@ -1,11 +1,12 @@
 #ifndef _ODOMETER_H
 #define _ODOMETER_H
 #include "encoder.h"
-#define WHEEL_DIAMETER 5.88
+#define WHEEL_DIAMETER 0.0588
+#define WHEEL_RADIUS ((WHEEL_DIAMETER)/2)
 #define FREQUENCY_ENCODER 50
-#define WHELL_GAPS 23.5
+#define WHELL_GAPS 0.235
 
-double computeSpeed(enum Encoder encoder, double encoderVal);
+double computeSpeed(enum Encoder encoder, uint32_t encoderVal);
 
 typedef struct PolarSpeedStruct
 {
